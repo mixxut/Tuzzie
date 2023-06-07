26cf652b54c261bda6df161200905fa709e5b490
mixxut <36286494+mixxut@users.noreply.github.com>
2023-06-07 14:13:49 -0500

    Add files via upload
    v1
    setting up dev environment. Unfortunately i was unable to get work done as the partition i am working on lacks the space to download react.

---
+++ App.js
@@ -0,0 +1,17 @@
+function MyButton() {
+    return (
+      <button>
+        button
+      </button>
+    );
+  }
+  
+  export default function MyApp() {
+    return (
+      <div>
+        <h1>Tuzzie Rodriguez</h1>
+        <MyButton />
+      </div>
+    );
+  }
+  
